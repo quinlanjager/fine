da6ca300d16bd50dd994b6acb67e903f45fa6a82
Quinlan Jager <quinlan.jager@gmail.com>
2026-03-05 08:29:03 -0800

    Add docs site

---
+++ docs/_config.ts
@@ -0,0 +1,22 @@
+import lume from "lume/mod.ts";
+import attributes from "lume/plugins/attributes.ts";
+import code_highlight from "lume/plugins/code_highlight.ts";
+import robots from "lume/plugins/robots.ts";
+import tailwindcss from "lume/plugins/tailwindcss.ts";
+import check_urls from "lume/plugins/check_urls.ts";
+import sitemap from "lume/plugins/sitemap.ts";
+import seo from "lume/plugins/seo.ts";
+
+const site = lume();
+
+site.use(attributes());
+site.use(code_highlight());
+site.use(robots());
+site.use(tailwindcss());
+site.use(check_urls());
+site.use(sitemap());
+site.use(seo());
+
+site.add("assets");
+
+export default site;
